fd6a4977faf5a0e73a85bb8e5275c50ce477b70b
sahilsheth <32227270+sahilsheth@users.noreply.github.com>
2019-03-04 17:20:10 -0800

    Add files via upload

---
+++ person.cpp
@@ -0,0 +1,80 @@
+#pragma once
+#include <string>
+#include "person.h"
+
+using namespace std;
+
+
+Person::Person()
+{
+  firstName= " ";
+  lastName = " ";
+  employeeID = 0;
+  companyName = " ";
+  hoursWorked = 0.0;
+  payRate = 0.0;
+}
+
+void Person::setFirstName(string fName)
+{
+   firstName = fName;
+
+}
+string Person::getFirstName()
+{
+   return firstName;
+
+}
+void Person::setLastName(string lName)
+{
+   lastName = lName;
+}
+string Person::getLastName()
+{
+  return lastName;
+}
+void Person::setEmployeeId(int id)
+{
+  employeeID = id;
+
+}
+int Person::getEmployeeId()
+{
+  return employeeID;
+}
+void Person::setCompanyName(string coName)
+{
+  companyName = coName;
+}
+string Person::getCompanyName()
+{
+  return companyName;
+
+
+}
+void Person::setPayRate(float rate)
+{
+  payRate = rate;
+}
+float Person::getPayRate()
+{
+   return payRate;
+}
+void Person::setHoursWorked(float hours)
+{
+  hoursWorked = hours;
+
+}
+float Person::getHoursWorked()
+{
+   return hoursWorked;
+}
+float Person::totalPay()
+{
+  return getPayRate() * getHoursWorked();
+
+}
+string Person::fullName()
+{
+  return firstName + " " + lastName;
+}
